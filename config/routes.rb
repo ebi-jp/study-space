909650fd6331293ca2e93a0747a7375c42fc0431
ebi-jp <57351121+ebi-jp@users.noreply.github.com>
2019-12-02 18:16:25 +0900

    ユーザー投稿一覧

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   # get 'posts/index'
   root "posts#index"
-  resources :users, only: [:create, :new,:edit ,:update]
+  resources :users, only: [:create, :new,:edit ,:update, :show]
   resources :posts, except: :index
 end
