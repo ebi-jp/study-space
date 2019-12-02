83985a3236fa8c8cd2e508549495284c4ce598e5
ebi-jp <57351121+ebi-jp@users.noreply.github.com>
2019-12-02 11:39:30 +0900

    投稿機能の実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,20 @@
 class PostsController < ApplicationController
+  
   def index
+    @posts = Post.all.includes(:user)
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    Post.create(post_params)
+    redirect_to root_path
+  end
+  private
+  def post_params
+    params.require(:post).permit(:title, :content,:image, :user,:tag).merge(user_id: current_user.id)
+
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
   # get 'posts/index'
   root "posts#index"
   resources :users, only: [:create, :new,:edit ,:update]
+  resources :posts, only: [:new, :create]
 end
