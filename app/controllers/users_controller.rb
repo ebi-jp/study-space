41b16773a5c9e7d61589d3e8e1464f178e9371ff
ebi-jp <57351121+ebi-jp@users.noreply.github.com>
2020-01-15 18:40:22 +0900

    投稿一覧の見た目

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
-    @posts = @user.posts
+    @posts = @user.posts.page(params[:page]).includes(:user).order("created_at DESC").per(10)
   end
 end
